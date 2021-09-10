f14c780f4a6768beb2713a88f6285465f93ff538
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-10 15:53:36 +0300

    add

---
+++ client/src/components/TodoList.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {Card, Header, Form, Input, Icon} from 'semantic-ui-react';
+
+let endpoint = "http://localhost:9090";
+
+class TodoList extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            task: "",
+            items: [],
+        };
+    }
+    ComponentDidMount(){
+        this.getTask();
+    }
+
+    render(){
+        return(
+            <>
+              <div className="row">
+                <Header className="header" as="h2" color="red">
+                   Todo List
+                </Header>
+              </div>
+            </>
+        )
+    }
+}
+
+export default TodoList;
---
+++ server/readme.md
@@ -0,0 +1,22 @@
+---
+stack: GO
+lang: all
+---
+
+## go module
+```
+go mod init folder-name or www.github.com/userName/repo-name
+```
+
+## Got a problem with runing? GOPATH should be set to
+```
+export GOPATH=$GOROOT
+unset GOROOT
+```
+
+##  GO111MODULE set "on" or "off"
+```
+go env -w GO111MODULE=off
+```
+
+---
